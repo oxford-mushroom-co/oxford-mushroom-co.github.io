66928a2953d148a1af31115d2d48f0a56028b248
Ben Chidgey <ben@feedmypixel.com>
2017-05-28 15:33:01 +0100

    fallback image work for mapbox map

--- src/assets/javascripts/mapbox/mapbox.js
+++ src/assets/javascripts/mapbox/mapbox.js
@@ -7,11 +7,18 @@ const renderMap = () => {
   const markerElement = document.createElement('div')
   const popupElement = document.createElement('div')
   const farmLongLat = [-1.2759754, 51.7389587]
+  const mapId = 'map'
+
+  // drop out img fallback
+  document.getElementById(mapId).innerHTML = ''
+
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v9',
     center: farmLongLat,
-    zoom: 12
+    zoom: 12,
+    trackResize: true,
+    failIfMajorPerformanceCaveat: true
   })
 
   map.addControl(new mapboxgl.NavigationControl())

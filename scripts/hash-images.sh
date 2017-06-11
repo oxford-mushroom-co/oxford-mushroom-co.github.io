4c841012bc86fd42a23bb3deb61f0364710ee204
Ben Chidgey <ben@feedmypixel.com>
2017-06-11 16:23:10 +0100

    add glob for all images

--- scripts/hash-images.sh
+++ scripts/hash-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 STYLESHEETS=( ./dist/assets/stylesheets/*.css )
-IMAGES=( ./dist/assets/images/*.jpg )
+IMAGES=( ./dist/assets/images/**/*.jpg ./dist/assets/images/*.png ) # TODO improve this glob
 HTML=( ./dist/*.html )
 
 function replaceAssets {

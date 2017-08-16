781ec932fded912c368a9f0bd61bffbd97821a33
Ben Chidgey <ben@feedmypixel.com>
2017-08-16 12:30:03 +0100

    update company name in titles

--- src/nunjucks/globals.js
+++ src/nunjucks/globals.js
@@ -1,6 +1,6 @@
 let globals = new Map()
 
-globals.set('siteName', 'The Oxford Mushroom Co.')
+globals.set('siteName', 'Oxford Mushroom Co.')
 globals.set('currentYear', (new Date()).getFullYear())
 
 const init = (env) => globals.forEach((value, key) => env.addGlobal(key, value))

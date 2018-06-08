181c170d2788484eee4574b5b713abe73e387fee
Ben Chidgey <ben@feedmypixel.com>
2018-06-08 16:26:32 +0100

    Nunjucks globals
    Add in the twitter user name for Oxford Mushrooms to nunjucks globals

--- src/nunjucks/globals.js
+++ src/nunjucks/globals.js
@@ -2,6 +2,7 @@ let globals = new Map()
 
 globals.set('siteName', 'Oxford Mushroom Co.')
 globals.set('currentYear', (new Date()).getFullYear())
+globals.set('twitterUserName', '@OxfordMushrooms')
 
 const init = (env) => globals.forEach((value, key) => env.addGlobal(key, value))
 

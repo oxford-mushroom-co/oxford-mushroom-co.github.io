8c33568510b6f27d072c048c2bb8bbf37c10c3e9
Ben Chidgey <ben@feedmypixel.com>
2019-01-03 21:30:07 +0000

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -o errexit
 echo "---- Running Deploy"
 
 npm run build && \
-git checkout master && \
+git checkout master && git pull origin master && \
 rm -rf assets/ && rm -f index.html && \
 mv dist/* . && \
 git add . && git commit && git push origin master && \
